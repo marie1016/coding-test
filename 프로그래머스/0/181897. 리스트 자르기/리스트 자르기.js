0c18e72f36378006dc6dc9179a6aa7948ec00cc2
marie1016 <kjo5281@gmail.com>
2026-01-07 23:49:17 +0900

    [level 0] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,12 @@
+function solution(n, slicer, num_list) {
+    if (n === 1) {
+      num_list = num_list.slice(0, slicer[1] + 1);
+    } else if (n === 2) {
+      num_list = num_list.slice(slicer[0]);
+    } else if (n === 3) {
+       num_list = num_list.slice(slicer[0], slicer[1] + 1);
+    } else if (n === 4) {
+       num_list = num_list.slice(slicer[0], slicer[1] +1).filter((_, i) => i % slicer[2] === 0);
+    }
+    return num_list;
+}
