d2934e72a4b0e3cf411a1258e3f65c522ecbcdea
marie1016 <kjo5281@gmail.com>
2026-02-24 23:27:35 +0900

    [level 1] Title: 직사각형 별찍기, Time: 47.57 ms, Memory: 32 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -0,0 +1,14 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    
+    let s = "";
+    for(let i = 0; i < a; i++) {
+        s += "*";
+    }
+    
+    for(let j = 0; j < b; j++) {
+        console.log(s);
+    }
+});
